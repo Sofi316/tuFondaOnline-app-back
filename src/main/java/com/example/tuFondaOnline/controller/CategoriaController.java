e23715e67ff925091da4ea878eaf3c2cddecb753
unknown <CETECOM@AOAWSB02LC0617.duoc.acad>
2025-11-27 14:02:47 -0300

    Correción de CategoriaController

--- src/main/java/com/example/tuFondaOnline/controller/CategoriaController.java
+++ src/main/java/com/example/tuFondaOnline/controller/CategoriaController.java
@@ -23,8 +23,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 @RequestMapping("/api/categorias")
@@ -43,6 +41,19 @@ public class CategoriaController {
     })
     public List<Categoria> getAll(){
         return categoriaService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Obtener una categoria por ID", description = "Obtiene una categoria específica por su ID")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Operación exitosa",
+            content = @Content(mediaType = "application/json",
+                schema = @Schema(implementation = Categoria.class))),
+        @ApiResponse(responseCode = "404", description = "Categoria no encontrada")
+    })
+    public Categoria getById(@Parameter(description = "ID de la categoría a obtener", required = true)
+        @PathVariable Long id){
+        return categoriaService.findById(id);
     }
 
     @PostMapping
@@ -67,6 +78,9 @@ public class CategoriaController {
         @RequestBody Categoria categoria){
         return categoriaService.save(categoria);    
     }
+
+    //Te falta el PUTMapping pajaron
+
     @DeleteMapping("/{id}")
     @Operation(summary = "Eliminar una categoria", description = "Elimina una categoria por su código")
     @ApiResponses(value = {
