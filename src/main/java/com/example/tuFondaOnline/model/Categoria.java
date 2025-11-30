e3382a50238fe948a21c78a87b6755275cd024a0
Romina <rom.hormazabal@duocuc.cl>
2025-11-30 17:56:23 -0300

    Categoria y publicación funcionando

---
+++ src/main/java/com/example/tuFondaOnline/model/Categoria.java
@@ -0,0 +1,26 @@
+package com.example.tuFondaOnline.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categoria")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Long idCategoria;
+    @Column(nullable=false, length=50)
+    private String nombre;
+}
